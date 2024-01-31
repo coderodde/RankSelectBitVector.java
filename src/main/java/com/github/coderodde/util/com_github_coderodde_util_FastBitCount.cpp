eef7cee0056345d20d2a3cfe9645dea6174eed97
Rodion Efremov <coderodd3@yandex.com>
2024-01-31 11:59:19 +0200

    Deleted the popcnt-based bit vector stuff.

--- src/main/java/com/github/coderodde/util/com_github_coderodde_util_FastBitCount.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "com_github_coderodde_util_FastBitCount.h"
-#include <intrin.h>
-
-JNIEXPORT jint JNICALL Java_com_github_coderodde_util_FastBitCount_popcnt(JNIEnv*, jclass, jlong value) {
-    return (jint) __popcnt64(value);
-}
